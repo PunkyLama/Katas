f5424a659bd5a470451cbea12642d60fc933b7c6
guillaume.le.roux9529@gmail.com <Monvm95ch>
2023-07-07 00:34:13 -0400

    pouet

--- HashFunction/HashCodeWPF/MainWindow.xaml.cs
+++ HashFunction/HashCodeWPF/MainWindow.xaml.cs
@@ -1,4 +1,70 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HashCodeWPF
+{
+    public partial class MainWindow : Window
+    {
+        public int numHashes;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            PopUp.Visibility = Visibility.Hidden;
+        }
+
+        private void btn_HashCode_Click(object sender, RoutedEventArgs e)
+        {
+            PopUp.Visibility = Visibility.Visible;
+        }
+
+        private async void btn_validate_Click(object sender, RoutedEventArgs e)
+        {
+            numHashes = int.Parse(tb_Hashage.Text);
+            btn_HashCode.IsEnabled = false;
+            PopUp.Visibility = Visibility.Hidden;
+
+            await Task.WhenAll(
+                Task.Run(() => Program.GenerateHashesAsync(Program.hashGroups, numHashes)),
+                Task.Run(() => Program.FindOccurrencesAsync(Program.hashGroups, numHashes))
+            );
+
+            btn_HashCode.IsEnabled = true;
+            DisplayResults();
+        }
+
+        private void DisplayResults()
+        {
+            tb_affichage.Text = "";
+            foreach (var group in Program.hashGroups)
+            {
+                if (group.Value.Count == numHashes)
+                {
+                    tb_affichage.Text += $"Hash: {group.Key}\n";
+                    foreach (var str in group.Value)
+                    {
+                        tb_affichage.Text += $"{str}\n";
+                    }
+                    tb_affichage.Text += "\n";
+                }
+            }
+        }
+
+        private void tb_Hashage_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
+
+/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -54,3 +120,4 @@ namespace HashCodeWPF
         }
     }
 }
+*/
--- HashFunction/HashCodeWPF/Program.cs
+++ HashFunction/HashCodeWPF/Program.cs
@@ -1,4 +1,148 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodeWPF
+{
+    public class Program
+    {
+        private static Random Randomizer = new Random();
+        public static ConcurrentDictionary<int, List<string>> hashGroups = new ConcurrentDictionary<int, List<string>>();
+        public static int numChar;
+
+        public static async Task FindOccurrencesAsync(ConcurrentDictionary<int, List<string>> hashGroups, int numHashes)
+        {
+            List<int> hashes = new List<int>(hashGroups.Keys);
+            foreach (int hash in hashes)
+            {
+                if (hashGroups[hash].Count == numHashes)
+                {
+                    Debug.WriteLine($"{numHashes} chaînes de caractères avec le même code de hachage {hash} ont été trouvées. Voici la liste des chaînes :");
+                    foreach (string StringInList in hashGroups[hash])
+                    {
+                        Debug.WriteLine(StringInList);
+                    }
+                    Stopwatch timer = Stopwatch.StartNew();
+                    TimeSpan timeTaken = timer.Elapsed;
+                    timer.Stop();
+                    Debug.WriteLine("Time taken : " + timeTaken.ToString(@"m\:ss\.fff"));
+                    return;
+                }
+            }
+        }
+
+        public static async Task GenerateHashesAsync(ConcurrentDictionary<int, List<string>> hashGroups, int numHashes)
+        {
+            while (true)
+            {
+                byte[] tmpSource;
+                var RString = RandomString();
+                tmpSource = ASCIIEncoding.ASCII.GetBytes(RString);
+                var HashedString = tmpSource.GetHashCode();
+                hashGroups.AddOrUpdate(HashedString, new List<string> { RString }, (key, oldValue) =>
+                {
+                    if (oldValue.Count < numHashes && !oldValue.Contains(RString))
+                    {
+                        oldValue.Add(RString);
+                    }
+                    return oldValue;
+                });
+                if (hashGroups[HashedString].Count == numHashes)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static string RandomString()
+        {
+            StringBuilder StringBuild = new StringBuilder();
+            for (int i = 0; i < 4; i++)
+            {
+                StringBuild.Append((char)Randomizer.Next((int)'a', ((int)'z') + 1));
+            }
+            return StringBuild.ToString();
+        }
+    }
+}
+
+/*using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodeWPF
+{
+    public class Program
+    {
+        private static Random Randomizer = new Random();
+        public static Dictionary<int, List<string>> hashGroups = new Dictionary<int, List<string>>();
+        public static int numChar;
+
+        public static async Task FindOccurrencesAsync(Dictionary<int, List<string>> hashGroups, int numHashes)
+        {
+            List<int> hashes = new List<int>(hashGroups.Keys);
+            foreach (int hash in hashes)
+            {
+                if (hashGroups[hash].Count == numHashes)
+                {
+                    Debug.WriteLine($"{numHashes} chaînes de caractères avec le même code de hachage {hash} ont été trouvées. Voici la liste des chaînes :");
+                    foreach (string StringInList in hashGroups[hash])
+                    {
+                        Debug.WriteLine(StringInList);
+                    }
+                    Stopwatch timer = Stopwatch.StartNew();
+                    TimeSpan timeTaken = timer.Elapsed;
+                    timer.Stop();
+                    Debug.WriteLine("Time taken : " + timeTaken.ToString(@"m\:ss\.fff"));
+                    return;
+                }
+            }
+        }
+
+        public static async Task GenerateHashesAsync(Dictionary<int, List<string>> hashGroups, int numHashes)
+        {
+            while (true)
+            {
+                byte[] tmpSource;
+                var RString = RandomString();
+                tmpSource = ASCIIEncoding.ASCII.GetBytes(RString);
+                var HashedString = tmpSource.GetHashCode();
+                lock (hashGroups)
+                {
+                    if (!hashGroups.ContainsKey(HashedString))
+                    {
+                        hashGroups[HashedString] = new List<string>();
+                    }
+                    if (hashGroups[HashedString].Count < numHashes && !hashGroups[HashedString].Contains(RString))
+                    {
+                        hashGroups[HashedString].Add(RString);
+                    }
+                }
+                if (hashGroups[HashedString].Count == numHashes)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static string RandomString()
+        {
+            StringBuilder StringBuild = new StringBuilder();
+            for (int i = 0; i < 4; i++)
+            {
+                StringBuild.Append((char)Randomizer.Next((int)'a', ((int)'z') + 1));
+            }
+            return StringBuild.ToString();
+        }
+    }
+}
+*/
+/*using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO.Packaging;
@@ -73,3 +217,4 @@ namespace HashCodeWPF
         }
     }
 }
+*/
